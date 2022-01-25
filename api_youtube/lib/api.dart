027afcaeb618bc011f57ec7c6c422f6cd9e9a9be
Wilson Junior <wilsonraj@unipam.edu.br>
2022-01-24 22:05:28 -0300

    Ativando a API do youtube

---
+++ api_youtube/lib/api.dart
@@ -0,0 +1,6 @@
+// ignore: constant_identifier_names
+const CHAVE_YOUTUBE_API = "AIzaSyCEoIFeGwKMc1pEfFXuSMJHgFg1kKzKOvg";
+// ignore: constant_identifier_names
+const CANAL = "UCt_4wzTQqmcUvemNkeO0plA";
+// ignore: constant_identifier_names
+const URL_BASE = "https://www.googleapis.com/youtube/v3/";
