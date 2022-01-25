d79e45d934625b15cd457bcb85b38370bfba9c81
Wilson Junior <wilsonraj@unipam.edu.br>
2022-01-24 23:31:07 -0300

    Convertendo os resultados retornados da API por meio do Factory

--- api_youtube/lib/api.dart
+++ api_youtube/lib/api.dart
@@ -1,4 +1,6 @@
 // ignore: import_of_legacy_library_into_null_safe
+import 'package:api_youtube/model/video.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -24,11 +26,15 @@ class Api {
 
     if(response.statusCode == 200){
       Map<String, dynamic> dadosJson = json.decode(response.body);
+      List<Video> videos = dadosJson["items"].map<Video>(
+        (map){
+          return Video.fromJson(map);
+        }
+      ).ToList();
       // ignore: avoid_print
       print("Resultado " + dadosJson[""]);
     }
     else{
-
     }
   }
 }
--- api_youtube/lib/model/video.dart
+++ api_youtube/lib/model/video.dart
@@ -1,9 +1,19 @@
 class Video {
-    String id = "";
-    String titulo = "";
-    String descricao = "";
-    String imagem = "";
-    String canal = "";
+  String id;
+  String titulo;
+  //String descricao;
+  String imagem;
+  String canal;
 
-    Video(this.id, this.titulo, this.descricao, this.imagem, this.canal);
+  Video({required this.id, required this.titulo, required this.imagem, required this.canal});
+
+  factory Video.fromJson(Map<String, dynamic> json){
+    return Video (
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      //descricao: json["snippet"]["description"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      canal: json["snippet"]["channelId"],
+    );
+  }
 }
